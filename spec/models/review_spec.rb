d4dc216a124212955fe946230ba15b74893d75b6
shiversamar <shiversamar@yahoo.com>
2016-12-01 11:32:15 -0800

    user are now able to post reviews to the review page with a map to show restaurant location + review model and feature rspec pass

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,18 +6,13 @@ RSpec.describe Review, type: :model do
     review1.restaurant_name = "Popeyes"
     review1.cuisine = "Southern"
     review1.dish = "Fried Chicken"
-    review1.price = "5"
-    review1.rating = "3"
+    review1.price = 5.0
+    review1.rating = 3
     review1.street = "1550 Market Street"
+    review1.city = "San Diego"
+    review1.state = "California"
+    review1.postalcode = "92116"
+    review1.description = "It was wonderful"
     expect(review1.save).to be true
   end
-  # it "Can be searched" do
-  # review2 = Review.find(review1.id)
-  # expect(review2.restaurant_name).to be "Popeyes"
-  # expect(review2.cuisine).to be "Southern"
-  # expect(review2.dish).to be "Fried Chicken"
-  # expect(review2.price).to eq "5"
-  # expect(review2.rating).to eq "3"
-  # expect(review2.street).to eq "1550 Market Street"
-# end
 end
