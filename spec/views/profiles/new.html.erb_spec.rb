95197573944a3c956ec776d1e22d847d438e83dd
christinabuster <christina.buster13@gmail.com>
2016-11-30 16:18:59 -0800

    create scaffolding for user profile

---
+++ spec/views/profiles/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "profiles/new", type: :view do
+  before(:each) do
+    assign(:profile, Profile.new(
+      :name => "MyString",
+      :about_me => "MyText"
+    ))
+  end
+
+  it "renders new profile form" do
+    render
+
+    assert_select "form[action=?][method=?]", profiles_path, "post" do
+
+      assert_select "input#profile_name[name=?]", "profile[name]"
+
+      assert_select "textarea#profile_about_me[name=?]", "profile[about_me]"
+    end
+  end
+end
