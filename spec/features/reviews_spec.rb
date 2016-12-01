941305287f310277636354531842836db2f0eddd
shiversamar <shiversamar@yahoo.com>
2016-12-01 10:59:43 -0800

    completed features review spec and worked on model spec + added create review to navbar

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -10,7 +10,7 @@ RSpec.feature "Reviews", type: :feature do
         expect(page). to have_content ("Welcome to Consumable")
       end
     end
-
+#
     Steps 'signup' do
       Given 'I that I am on the sign up page' do
         visit '/'
@@ -21,41 +21,15 @@ RSpec.feature "Reviews", type: :feature do
         fill_in('Email', :with => 'test@gmail.com')
         fill_in('user_password', :with => 'password')
         fill_in('user_password_confirmation', :with => 'password')
-        expect {
         click_button('Sign up')
-      }.to change(User, :count).by(1)
         expect(current_path).to eq('/')
         expect(page).to have_content("Consumable")
         expect(page).to have_content ("@")
-        
+
       end
-
-
-
-      #
-      # Then 'I am logged out' do
-      #   click_link('Logout')
-      # end
-      #
-      # Then 'I am on the login page' do
-      #   click_link('nav_log_in')
-      #   expect(page).to have_content ("Forgot your password")
-      # end
-      #
-      # Then 'I can log in' do
-      #   fill_in('Email', :with => 'test@gmail.com')
-      #   fill_in('user_password', :with => 'password')
-      # end
-
-
-      # Then 'I will be on the logg' do
-      # #   expect(current_path).to eq('/')
-      # expect(page).to have_content("Concerns?")
-      # expect(page).to have_content ("test@gmail.com")
-      # end
     end
   end
-
+#
   context 'Writing a review for a new dish' do
     Steps 'input restaurant/dish information' do
       Given 'I am on the user review page' do
@@ -73,10 +47,9 @@ RSpec.feature "Reviews", type: :feature do
         fill_in('Postalcode', :with => '92123')
         fill_in('Description', :with => 'The chicken was very tasty')
         fill_in('User', :with => '1')
-        click_button('Create Review')
-        expect(current_path).to eq('/reviews')
+        find('#create_review').click
+        expect(page).to have_content('The chicken was very tasty')
       end
     end
   end
-
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  it "has a lot of stuff" do
+  it "has a saved review" do
     review1 = Review.new
     review1.restaurant_name = "Popeyes"
     review1.cuisine = "Southern"
-    review1.dish = "Fried chicken"
+    review1.dish = "Fried Chicken"
     review1.price = "5"
     review1.rating = "3"
-    review1.street = ""
-    expect(car1.save).to be true
-    car2 = Car.find(car1.id)
-    expect(car2.year).to be 1999
-    expect(car2.make).to eq "Tata"
-    expect(car2.license_plate).to eq "ABC123"
+    review1.street = "1550 Market Street"
+    expect(review1.save).to be true
   end
+  # it "Can be searched" do
+  # review2 = Review.find(review1.id)
+  # expect(review2.restaurant_name).to be "Popeyes"
+  # expect(review2.cuisine).to be "Southern"
+  # expect(review2.dish).to be "Fried Chicken"
+  # expect(review2.price).to eq "5"
+  # expect(review2.rating).to eq "3"
+  # expect(review2.street).to eq "1550 Market Street"
+# end
 end
