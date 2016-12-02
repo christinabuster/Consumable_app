dab5f0108f7a3df179ebc2bc25e5e4edee9cf33c
cpena002 <carlos_enrique_pena@yahoo.com>
2016-12-02 15:11:08 -0800

    after signing up, page will redirect to user profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def after_sign_in_path_for (resources)
-    profiles_path(current_user)
+    profiles_path
   end
 end
