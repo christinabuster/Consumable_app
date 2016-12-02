e8d6a5deccb35c3107056adf2e629f6cfbfae220
cpena002 <carlos_enrique_pena@yahoo.com>
2016-12-02 13:45:09 -0800

    search by location

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,12 @@
 class SearchController < ApplicationController
 
   def search_by
+    @reviews = Review.all
   end
 
+  def location
+    @reviews = Review.where(postalcode: params[:postal])
+  end
 
   # def cuisine
   # end
