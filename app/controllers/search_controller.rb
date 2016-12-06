66a03ffceac910497cf3b3425fd763d48c95561a
christinabuster <christina.buster13@gmail.com>
2016-12-06 11:13:35 -0800

    wip search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,23 +2,59 @@ class SearchController < ApplicationController
   # search by one criteria at a time
   def new_search
 
-    if !params[:postal].nil? && !params[:postal].strip.empty?
-      @search_results = Review.search(postalcode: params[:postal])
-    elsif !params[:cuisine].nil? && !params[:cuisine].strip.empty?
-      @search_results = Review.search(cuisine: params[:cuisine])
-    elsif !params[:price].nil? && !params[:price].strip.empty?
+    if !params[:cuisine].nil? && !params[:cuisine].strip.empty?
+     # search by cuisine only
+     @search_results = Review.search(cuisine: params[:cuisine])
 
-      if params[:price] == '10'
-        @search_results = Review.where(price: 0..params[:price].to_i)
-      elsif params[:price] == '20'
-        @search_results = Review.where(price: 10..params[:price].to_i)
-      elsif params[:price] == '21+'
-        @search_results = Review.where(price: 20..100000)
-      end
-    elsif !params[:rating].nil? && !params[:rating].strip.empty?
-      @search_results = Review.where(rating: params[:rating])
     else
-      @search_results = Review.all
+     @search_results = Review.all
+
+    end
+
+    if !params[:postal].nil? && !params[:postal].strip.empty?
+      # search by location only
+      @search_results = @search_results.search(postalcode: params[:postal])
+
+      # search by location and cuisine; chained location.search and cuisine.search
+      # @search_results = Review.search(postalcode: params[:postal]).search(cuisine: params[:cuisine])
+      #
+      # #search by location, cuisine, and rating
+      # @search_results = Review.search(postalcode: params[:postal]).search(cuisine: params[:cuisine]).where(rating: params[:rating])
+    end
+
+
+
+    #search by rating
+    if !params[:rating].nil? && !params[:rating].strip.empty?
+      @search_results = @search_results.where(rating: params[:rating])
+
+    # search by price
+    elsif !params[:price].nil? && !params[:price].strip.empty?
+     if params[:price] == '10'
+       @search_results = @search_results.where(price: 0..params[:price].to_i)
+     elsif params[:price] == '20'
+       @search_results = @search_results.where(price: 10..params[:price].to_i)
+     elsif params[:price] == '30'
+       @search_results = @search_results.where(price: 20..params[:price].to_i)
+     elsif params[:price] == '41 +'
+       @search_results = @search_results.where(price: 30..100000)
+     end # if params == 10
+
+    #search by price & rating
+    elsif !params[:price].nil? && !params[:price].strip.empty?
+     if params[:price] == '10'
+       @search_results = @search_results.where(price: 0..params[:price].to_i).where(rating: params[:rating])
+     elsif params[:price] == '20'
+       @search_results = @search_results.where(price: 10..params[:price].to_i).where(rating: params[:rating])
+     elsif params[:price] == '30'
+       @search_results = @search_results.where(price: 20..params[:price].to_i).where(rating: params[:rating])
+     elsif params[:price] == '41 +'
+       @search_results = @search_results.where(price: 30..100000).where(rating: params[:rating])
+     end # if params == 10
+
+    # show all ratings
+    else
+      #@search_results = Review.all
     end
     render :search_by
   end #def new_search
