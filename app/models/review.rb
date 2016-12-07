6f60cfb64b0a07e7a4eea6b72ca7a7355f486ca1
shiversamar <shiversamar@yahoo.com>
2016-12-07 11:53:33 -0800

    resized images

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   geocoded_by :street
   after_validation :geocode
-  has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+  has_attached_file :image, :styles => { :small => "100x100", :medium => "150x150>", :large => "200x200" }
   validates_attachment :image,
   content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
   size: { in: 0..10.megabytes }
